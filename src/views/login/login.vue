f9248b05361e411aa17703ee6ad5141670e67e16
lhl <1638731192@qq.com>
2018-05-25 17:46:31 +0800

    ---

---
+++ src/views/login/login.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="login-box">
+    login
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'login',
+        data () {
+            return {
+
+            }
+        },
+        methods:{
+
+        },
+        computed:{
+
+        },
+        mounted(){
+
+        },
+        watch:{
+          // 如果 `某个属性` 发生改变，这个属性对应的函数就会运行
+        }
+    }
+</script>
+<style scoped>
+
+</style>
